d5e2e4a5b1a1170a311568d7fa8313449a89e1f7
gmoniava <moniava.g@gmail.com>
2025-06-11 21:35:22 +0400

    add post

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ export default async function Page(props) {
     <div>
       {" "}
       <h1 className="mb-4 text-2xl font-semibold ">About</h1>
-      <p className="mb-4">{`Explore my writings on software engineering, philosophy, psychology, and more.`}</p>
+      <p className="mb-4">{`Software developer from Tbilisi, Georgia. On this web site you can explore my writings on software engineering, philosophy, psychology, and more.`}</p>
     </div>
   );
 }
--- app/components/client/posts.tsx
+++ app/components/client/posts.tsx
@@ -94,36 +94,37 @@ export default function BlogPosts({ blogs }: any) {
           </div>
         </Link>
       ))}
-
-      {/* Pagination */}
-      <div className="flex items-center justify-center mt-6">
-        <button
-          onClick={() => {
-            setPage(page - 1);
-            updateURLParams(tags, page - 1);
-          }}
-          disabled={page <= 1}
-          className="px-3 py-1 cursor-pointer text-sm rounded disabled:opacity-50"
-        >
-          <span className="inline-block font-bold cursor-pointer select-none hover:text-gray-700">&larr;</span>
-          Prev
-        </button>
-        <span className="text-center text-sm text-neutral-600 dark:text-neutral-400">
-          {" "}
-          {`${totalFilteredPages > 0 ? page : 0}/${totalFilteredPages}`}
-        </span>
-        <button
-          onClick={() => {
-            setPage(page + 1);
-            updateURLParams(tags, page + 1);
-          }}
-          disabled={page >= totalFilteredPages}
-          className="px-3 py-1 text-sm cursor-pointer rounded disabled:opacity-50"
-        >
-          Next
-          <span className="inline-block select-none">&rarr;</span>
-        </button>
-      </div>
+      {/* Show pagination controls if there are multiple pages */}
+      {totalFilteredPages > 1 && (
+        <div className="flex items-center justify-center mt-6">
+          <button
+            onClick={() => {
+              setPage(page - 1);
+              updateURLParams(tags, page - 1);
+            }}
+            disabled={page <= 1}
+            className="px-3 py-1 cursor-pointer text-sm rounded disabled:opacity-50"
+          >
+            <span className="inline-block font-bold cursor-pointer select-none hover:text-gray-700">&larr;</span>
+            Prev
+          </button>
+          <span className="text-center text-sm text-neutral-600 dark:text-neutral-400">
+            {" "}
+            {`${page}/${totalFilteredPages}`}
+          </span>
+          <button
+            onClick={() => {
+              setPage(page + 1);
+              updateURLParams(tags, page + 1);
+            }}
+            disabled={page >= totalFilteredPages}
+            className="px-3 py-1 text-sm cursor-pointer rounded disabled:opacity-50"
+          >
+            Next
+            <span className="inline-block select-none">&rarr;</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
