7b16e08099406ec2a35c65af3dfc402fc4d4f1c0
gmoniava <moniava.g@gmail.com>
2025-06-14 22:56:10 +0400

    add some info in about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,55 @@ export default async function Page(props) {
     <div>
       {" "}
       <h1 className="mb-4 text-2xl font-semibold ">About</h1>
-      <p className="mb-4">{`Software developer from Tbilisi, Georgia. On this web site you can explore my writings on software engineering, philosophy, psychology, and more.`}</p>
+      <p className="mb-4">{`I am software developer (mostly front end) from Tbilisi, Georgia. On this web site you can explore my writings on software engineering, philosophy, psychology, and more.`}</p>
+      <div></div>
+      <div style={{ display: "inline-flex", alignItems: "center", gap: "12px" }}>
+        <a
+          href="https://stackoverflow.com/users/3963067/gmoniava"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 dark:text-blue-400"
+          style={{ display: "flex", alignItems: "center", gap: "4px" }}
+        >
+          Stack Overflow
+        </a>
+
+        {/* Vertical Divider */}
+        <div
+          style={{
+            borderLeft: "1px solid #ccc",
+            height: "20px",
+          }}
+        />
+
+        <a
+          href="https://github.com/gmoniava"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 dark:text-blue-400"
+          style={{ display: "flex", alignItems: "center", gap: "4px" }}
+        >
+          GitHub
+        </a>
+
+        {/* Vertical Divider */}
+        <div
+          style={{
+            borderLeft: "1px solid #ccc",
+            height: "20px",
+          }}
+        />
+
+        <a
+          href="https://www.linkedin.com/in/giorgi-moniava-1797a42"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 dark:text-blue-400"
+          style={{ display: "flex", alignItems: "center", gap: "4px" }}
+        >
+          LinkedIn
+        </a>
+      </div>
     </div>
   );
 }
