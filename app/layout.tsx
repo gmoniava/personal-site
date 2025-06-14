bd64340631db074823ba01a4c225447facd0316e
gmoniava <moniava.g@gmail.com>
2025-06-14 09:47:06 +0400

    update post

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressHydrationWarning
       className={cx("text-black bg-white dark:text-white dark:bg-black", GeistSans.variable, GeistMono.variable)}
     >
-      <body className="antialiased max-w-3xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-2xl mx-4 mt-8 lg:mx-auto">
         <main className="mt-6 flex flex-col px-2 md:px-0">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="personal-website-theme">
             <Navbar />
