57fad26b0da10a041e63d703e74769dcb3d4f2f9
gmoniava <moniava.g@gmail.com>
2025-07-05 22:46:39 +0400

    fix prop

--- app/components/server/posts.tsx
+++ app/components/server/posts.tsx
@@ -37,7 +37,7 @@ export default function Page() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           >
