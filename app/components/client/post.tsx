5aa4fd047d5d9b553a1b8ddd906f374eb45fb673
gmoniava <moniava.g@gmail.com>
2025-08-11 15:00:50 +0400

    change posts layout

--- app/components/client/post.tsx
+++ app/components/client/post.tsx
@@ -17,27 +17,24 @@ type BlogPostPreviewProps = {
 export default function Post({ post }: BlogPostPreviewProps) {
   return (
     <Link key={post.slug} className="w-full" href={`/blog/${post.slug}`}>
-      <div className="flex flex-col sm:flex-row gap-1 sm:gap-4">
-        <p className="text-neutral-600 dark:text-neutral-400 w-[110px] flex-shrink-0">
+      <div className="flex flex-col gap-1 sm:gap-0.5 mb-2">
+        <p className="text-neutral-900 dark:text-neutral-100">{post.metadata.title}</p>
+        <p className="text-sm text-neutral-600 dark:text-neutral-400 w-[110px] flex-shrink-0">
           {formatDate(post.metadata.date, false)}
         </p>
 
-        <div className="flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100">{post.metadata.title}</p>
-
-          {post.metadata.tags && post.metadata.tags.length > 0 && (
-            <div className="flex flex-wrap gap-1 mt-1">
-              {post.metadata.tags.map((tag: string) => (
-                <span
-                  key={tag}
-                  className="bg-gray-200 dark:bg-neutral-700 text-gray-800 dark:text-gray-300 text-sm px-2 py-0.5 rounded"
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
-          )}
-        </div>
+        {post.metadata.tags && post.metadata.tags.length > 0 && (
+          <div className="flex flex-wrap gap-1 mt-1">
+            {post.metadata.tags.map((tag: string) => (
+              <span
+                key={tag}
+                className="bg-gray-200 dark:bg-neutral-700 text-gray-800 dark:text-gray-300 text-sm px-2 py-0.5 rounded"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
       </div>
     </Link>
   );
